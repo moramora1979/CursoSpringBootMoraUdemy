aca2849d4fb5a7694c85ff5f32981574cd7c67d7
moramora1979 <ciromora.estudio@gmail.com>
2021-08-08 19:00:00 -0500

    Seccion 13

---
+++ Seccion 13 Spring Security/2_check_role_controller]/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
@@ -0,0 +1,60 @@
+package com.bolsadeideas.springboot.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.bolsadeideas.springboot.app.auth.handler.LoginSuccessHandler;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	
+	@Autowired
+	private LoginSuccessHandler loginSuccessHandler;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**","/listar").permitAll()
+		.antMatchers("/ver/**").hasAnyRole("USER")
+		.antMatchers("/uploads/**").hasAnyRole("USER")
+		.antMatchers("/form/**").hasAnyRole("ADMIN")
+		.antMatchers("/eliminar/**").hasAnyRole("ADMIN")
+		.antMatchers("/factura/**").hasAnyRole("ADMIN")
+		.anyRequest().authenticated()
+		.and()
+		.formLogin().successHandler(loginSuccessHandler)
+		.loginPage("/login")
+		.permitAll()
+		.and().logout().permitAll().and().exceptionHandling().accessDeniedPage("/error_403");
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Autowired
+	public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception {
+
+		PasswordEncoder encoder = passwordEncoder();
+
+		// UserBuilder users = User.builder().passwordEncoder(password -> {
+		// return encoder.encode(password);});
+
+		// UserBuilder users = User.builder().passwordEncoder(password ->
+		// encoder.encode(password));
+
+		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+		builder.inMemoryAuthentication()
+		.withUser(users.username("admin").password("12345").roles("ADMIN","USER"))
+		.withUser(users.username("ciro").password("12345").roles("USER"));
+	}
+}
---
+++ Seccion 13 Spring Security/3_Capturar_User_Loguin y Validar Roles/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/LoginController.java
@@ -0,0 +1,37 @@
+package com.bolsadeideas.springboot.app.controllers;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/login")
+	public String login(@RequestParam(value="error", required=false) String error
+			,@RequestParam(value="logout", required=false) String logout
+			,Model model, Principal principal, RedirectAttributes flash) {
+		
+		if (principal != null) {
+			flash.addFlashAttribute("info", "Parce usted ya tiene sesion iniciada....");
+			return "redirect:/";
+		}
+		
+		
+		if(error != null) {
+			model.addAttribute("error", "Error en el login: Nombre de Usuario Contraseña incorrecta, por favor volver a intentar.");
+			
+		}
+		
+		if(logout != null) {
+			model.addAttribute("success", "Has cerrado sesión con éxito..");
+			
+		}
+		
+		return "login";
+	}
+}
